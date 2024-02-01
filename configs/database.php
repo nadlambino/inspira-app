1ead318916fce87bfbbb6596b37f2512057a511d
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-02-01 19:02:02 +0800

    chore: composer update

--- configs/database.php
+++ configs/database.php
@@ -9,6 +9,10 @@ return [
 			'host'     => env('DB_HOST', 'localhost'),
 			'port'     => env('DB_PORT', 3306),
 			'database' => env('DB_DATABASE', 'inspira'),
+			'credentials' => [
+				'username' => env('DB_USERNAME', 'root'),
+				'password' => env('DB_PASSWORD', ''),
+			],
 
 			/**
 			 * Database connection string
@@ -23,11 +27,6 @@ return [
 			 */
 			'commands' => [],
 
-			'credentials' => [
-				'username' => env('DB_USERNAME', 'root'),
-				'password' => env('DB_PASSWORD', ''),
-			],
-
 			/**
 			 * PDO attributes
 			 */
@@ -39,6 +38,10 @@ return [
 			'host'     => env('DB_HOST', 'localhost'),
 			'port'     => env('DB_PORT', 5432),
 			'database' => env('DB_DATABASE', 'inspira'),
+			'credentials' => [
+				'username' => env('DB_USERNAME', 'postgres'),
+				'password' => env('DB_PASSWORD', ''),
+			],
 
 			/**
 			 * Database connection string
@@ -52,41 +55,6 @@ return [
 			 * Refer to the official documentation of PostgreSQL for the command you need
 			 */
 			'commands' => [],
-
-			'credentials' => [
-				'username' => env('DB_USERNAME', 'postgres'),
-				'password' => env('DB_PASSWORD', ''),
-			],
-
-			/**
-			 * PDO attributes
-			 */
-			'attributes' => []
-		],
-
-		'sqlsrv' => [
-			'driver'   => 'sqlsrv',
-			'host'     => env('DB_HOST', 'localhost'),
-			'port'     => env('DB_PORT', 1433),
-			'database' => env('DB_DATABASE', 'inspira'),
-
-			/**
-			 * Database connection string
-			 * This will be use instead of the given host, port, and database
-			 */
-			'database_url' => env('DB_URL', ''),
-
-			/**
-			 * SQL commands that will be executed upon creating the PDO connection
-			 * For example: setting charset, time_zone, session sql_mode, etc.
-			 * Refer to the official documentation of SQLServer for the command you need
-			 */
-			'commands' => [],
-
-			'credentials' => [
-				'username' => env('DB_USERNAME', 'sa'),
-				'password' => env('DB_PASSWORD', ''),
-			],
 
 			/**
 			 * PDO attributes
@@ -110,11 +78,16 @@ return [
 			'attributes' => []
 		],
 
-		'oci' => [
-			'driver'   => 'oci',
+		/** SQL Server is not yet supported!!! */
+		'sqlsrv' => [
+			'driver'   => 'sqlsrv',
 			'host'     => env('DB_HOST', 'localhost'),
-			'port'     => env('DB_PORT', 1521),
+			'port'     => env('DB_PORT', 1433),
 			'database' => env('DB_DATABASE', 'inspira'),
+			'credentials' => [
+				'username' => env('DB_USERNAME', 'sa'),
+				'password' => env('DB_PASSWORD', ''),
+			],
 
 			/**
 			 * Database connection string
@@ -125,14 +98,9 @@ return [
 			/**
 			 * SQL commands that will be executed upon creating the PDO connection
 			 * For example: setting charset, time_zone, session sql_mode, etc.
-			 * Refer to the official documentation for the command you need
+			 * Refer to the official documentation of SQLServer for the command you need
 			 */
 			'commands' => [],
-
-			'credentials' => [
-				'username' => env('DB_USERNAME', 'SYS'),
-				'password' => env('DB_PASSWORD', ''),
-			],
 
 			/**
 			 * PDO attributes
