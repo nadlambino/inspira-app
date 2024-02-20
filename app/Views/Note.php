6825606a17e5e2a3ce11c77bb694b64eac1b6c80
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-02-20 18:07:44 +0800

    ref: update note component

--- app/Views/Note.php
+++ app/Views/Note.php
@@ -6,5 +6,9 @@ use Inspira\View\Components\Component;
 
 class Note extends Component
 {
+    protected bool $mergeProps = true;
+    
+    public ?string $title = null;
+
     public ?string $note = null;
 }
--- assets/views/pages/home.php
+++ assets/views/pages/home.php
@@ -3,7 +3,7 @@
 <!-- block contents -->
 <section class="flex flex-col gap-3 md:justify-center item-center h-screen text-center">
     <app-welcome></app-welcome>
-    <app-note class="note info mt-10" title="Disclaimer" note="This is a personal project of mine. It is meant for fun and educational purposes only."></app-note>
+    <app-note class="note info mt-10" title="Disclaimer" note="This is a personal project of mine. It is meant for fun and educational purposes only." />
     <app-note class="note warning mt-5" title="Warning">
         <p>
             This project is still in progress. Some pages and features are not yet available.
